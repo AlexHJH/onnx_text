d6074862355d3f1f51f96630a59660978e2b3ec2
Alex <30765323+AlexHJH@users.noreply.github.com>
2022-03-05 12:26:22 +0800

    Update onnx_text.py

--- onnx_text.py
+++ onnx_text.py
@@ -24,65 +24,77 @@ GRAPH_PROFILE_FORMAT = \
 >
 '''
 
+
 def _format_str(ori_str):
     number_str = [str(i) for i in range(0, 10)]
     if ori_str[0] in number_str:
         ori_str = "n{}".format(ori_str)
-    
+
     return re.sub(r'[\.\{\}\[\]\<\>\-]', "_", ori_str)
 
-def _tensor_type_to_str(tensor_type, name):
-    declare_dtype = onnx.mapping.STORAGE_TENSOR_TYPE_TO_FIELD[tensor_type.elem_type].split("_")[0]
-    declare_shape = [di.dim_param if di.dim_value < 0 else str(di.dim_value) for di in tensor_type.shape.dim]
 
-    declare_shape = "" if len(declare_shape) < 1 else "[{}]".format(", ".join(declare_shape))
+def _tensor_type_to_str(tensor_type, name):
+    declare_dtype = onnx.mapping.STORAGE_TENSOR_TYPE_TO_FIELD[tensor_type.elem_type].split("_")[
+        0]
+    declare_shape = [di.dim_param if di.dim_value < 0 else str(
+        di.dim_value) for di in tensor_type.shape.dim]
+
+    declare_shape = "" if len(declare_shape) < 1 else "[{}]".format(
+        ", ".join(declare_shape))
 
     return "{}{} {}".format(declare_dtype, declare_shape, name)
 
+
 def _tensor_to_str(tensor):
     tensor_bytes = BytesIO()
-    declare_dtype = onnx.mapping.STORAGE_TENSOR_TYPE_TO_FIELD[tensor.data_type].split("_")[0]
+    declare_dtype = onnx.mapping.STORAGE_TENSOR_TYPE_TO_FIELD[tensor.data_type].split("_")[
+        0]
 
     declare_shape = [str(di) for di in tensor.dims]
-    declare_shape = "" if len(declare_shape) < 1 else "[{}]".format(", ".join(declare_shape))
-    
+    declare_shape = "" if len(declare_shape) < 1 else "[{}]".format(
+        ", ".join(declare_shape))
+
     tensor_data = np.reshape(np.frombuffer(
-            tensor.raw_data, dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[tensor.data_type]), tensor.dims)
-        
-    tensor_data_str = str(tensor_data.flatten())
+        tensor.raw_data, dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[tensor.data_type]), tensor.dims)
+
+    tensor_data_str = str(list(tensor_data.flatten()))
 
     if tensor_data_str[0] == "[":
         tensor_data_str = tensor_data_str[1:-1]
 
-    tensor_bytes.write("{}{} = ".format(declare_dtype, declare_shape).encode('utf-8'))
+    tensor_bytes.write("{}{} = ".format(
+        declare_dtype, declare_shape).encode('utf-8'))
     tensor_bytes.write("{".encode('utf-8'))
     tensor_bytes.write(tensor_data_str.encode('utf-8'))
     tensor_bytes.write("}".encode('utf-8'))
     return tensor_bytes.getvalue().decode('utf-8')
 
+
 ATTRIBUTEPROTO_TO_STR_FUNC = {
     onnx_ml_pb2.AttributeProto.INT: ["i", str],
-    onnx_ml_pb2.AttributeProto.INTS: ["ints", lambda x : "[{}]".format(", ".join([str(xi) for xi in x]))],
+    onnx_ml_pb2.AttributeProto.INTS: ["ints", lambda x: "[{}]".format(", ".join([str(xi) for xi in x]))],
 
     onnx_ml_pb2.AttributeProto.FLOAT: ["f", str],
-    onnx_ml_pb2.AttributeProto.FLOATS: ["floats", lambda x : "[{}]".format(", ".join([str(xi) for xi in x]))],
+    onnx_ml_pb2.AttributeProto.FLOATS: ["floats", lambda x: "[{}]".format(", ".join([str(xi) for xi in x]))],
 
     onnx_ml_pb2.AttributeProto.STRING: ["s", str],
-    onnx_ml_pb2.AttributeProto.STRINGS: ["strings", lambda x : "[{}]".format(", ".join(['"{}"'.format(xi) for xi in x]))],
+    onnx_ml_pb2.AttributeProto.STRINGS: ["strings", lambda x: "[{}]".format(", ".join(['"{}"'.format(xi) for xi in x]))],
 
     onnx_ml_pb2.AttributeProto.TENSOR: ["t", _tensor_to_str],
-    onnx_ml_pb2.AttributeProto.TENSORS: ["tensors", lambda x : "[{}]".format(", ".join([_tensor_to_str(xi) for xi in x]))],
+    onnx_ml_pb2.AttributeProto.TENSORS: ["tensors", lambda x: "[{}]".format(", ".join([_tensor_to_str(xi) for xi in x]))],
 }
 
-def _node_to_str(node, bytes_io): 
-    input_info_str  = ", ".join([_format_str(i) for i in node.input])
-    output_info_str  = ", ".join([_format_str(i) for i in node.output])
 
-    bytes_io.write("    {} = {}".format(output_info_str, node.op_type).encode('utf-8'))
+def _node_to_str(node, bytes_io):
+    input_info_str = ", ".join([_format_str(i) for i in node.input])
+    output_info_str = ", ".join([_format_str(i) for i in node.output])
+
+    bytes_io.write("    {} = {}".format(
+        output_info_str, node.op_type).encode('utf-8'))
 
     if len(node.attribute) < 1:
         pass
-        #bytes_io.write("({})\n\n".format(output_info_str).encode('utf-8'))
+        # bytes_io.write("({})\n\n".format(output_info_str).encode('utf-8'))
     else:
         bytes_io.write("<\n".encode('utf-8'))
         for i, attri in enumerate(node.attribute):
@@ -102,7 +114,6 @@ def _node_to_str(node, bytes_io):
                 bytes_io.write(",\n".encode('utf-8'))
 
     bytes_io.write("({})\n\n".format(input_info_str).encode('utf-8'))
-
 
 
 def onnx_model_textual(onnx_model):
@@ -133,8 +144,13 @@ def onnx_model_textual(onnx_model):
         graph_output_dict[_format_str(item.name)] = item.type.tensor_type
 
     for item in onnx_model.graph.initializer:
-        graph_init_dict[_format_str(item.name)] = np.reshape(np.frombuffer(
-            item.raw_data, dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[item.data_type]), item.dims)
+        if len(item.raw_data) == 0:
+            graph_init_dict[_format_str(item.name)] = np.reshape(np.array(item.__getattribute__(
+                onnx.mapping.STORAGE_TENSOR_TYPE_TO_FIELD[item.data_type])).astype(onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[item.data_type]),
+                item.dims)
+        else:
+            graph_init_dict[_format_str(item.name)] = np.reshape(np.frombuffer(
+                item.raw_data, dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[item.data_type]), item.dims)
 
     value_input_names = set(graph_input_dict.keys()) - \
         set(graph_init_dict.keys())
@@ -174,18 +190,26 @@ def onnx_model_textual(onnx_model):
 if __name__ == '__main__':
     onnx_model_path = "mobile_centernet_v3_large.onnx"
 
+    # textual
     onnx_model = onnx.load_model(onnx_model_path)
 
     onnx_model_txt, graph_init_dict = onnx_model_textual(onnx_model)
 
-    with open("deploy.onnx.txt", 'w') as fp:
+    with open("deploy.onntxt", 'w') as fp:
         fp.write(onnx_model_txt)
         fp.close()
-    
+
     with open("deploy.pkl", 'wb') as fp:
         pickle.dump(graph_init_dict, fp)
+        fp.close()
+
+    # paser
+    with open("deploy.onntxt", 'r') as fp:
+        onnx_model_txt = fp.read()
         fp.close()
 
     onnx_model_new = onnx.parser.parse_model(onnx_model_txt)
 
     print(onnx_model_new)
+    
+    onnx.save_model(onnx_model_new, "onnx_model_from_text.onnx")
